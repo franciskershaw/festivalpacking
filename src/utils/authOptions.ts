8037b9373e8f05887d2eab82204d2d5a3e3a7f50
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-04-23 21:40:33 +0100

    Install and configure next-auth

---
+++ src/utils/authOptions.ts
@@ -0,0 +1,37 @@
+import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID ?? '',
+      clientSecret: process.env.GITHUB_SECRET ?? '',
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+  ],
+  // callbacks: {
+  //   // Invoked on successful signin
+  //   async signIn({ profile }) {
+  //     // 1. Connect to database
+  //     // 2. Check if user exists
+  //     // 3 If not, then add user to database
+  //     // 4. Return true to allow sign in
+  //   },
+  //   // Modifies the session object
+  //   async session({ session }) {
+  // 		// 1. Get user from database
+  // 		// 2. Assign the user id to the session
+  // 		// 3. Return sesion
+  // 	},
+  // },
+};
