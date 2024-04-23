b84b18191ff359f398ca20b90f75d0b29fce6380
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-04-23 22:14:31 +0100

    Log a user in and out using next-auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,48 @@
+'use client';
+
+import {
+  signIn,
+  signOut,
+  useSession,
+  getProviders,
+  ClientSafeProvider,
+  LiteralUnion,
+} from 'next-auth/react';
+import { useState, useEffect } from 'react';
+
+type ProviderType = LiteralUnion<'Google' | 'GitHub' | string, string>;
+
+type ProviderInfo = Record<ProviderType, ClientSafeProvider> | null;
+
 export default function Home() {
+  const { data: session } = useSession();
+  const [providers, setProviders] = useState<ProviderInfo>(null);
+
+  useEffect(() => {
+    const setAuthProviders = async () => {
+      const res = await getProviders();
+      setProviders(res);
+    };
+
+    setAuthProviders();
+  }, []);
+
   return (
-    <main className="">
-      <h1>Hello</h1>
+    <main className="flex justify-center items-center h-screen border gap-4">
+      {providers && !session ? (
+        Object.values(providers).map((provider, index) => (
+          <button
+            key={index}
+            onClick={() => signIn(provider.id)}
+            className="border py-2 px-4 text-2xl">
+            {provider.name}
+          </button>
+        ))
+      ) : (
+        <button onClick={() => signOut()} className="border py-2 px-4 text-2xl">
+          Logout
+        </button>
+      )}
     </main>
   );
 }
