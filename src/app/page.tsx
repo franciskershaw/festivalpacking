b0d9a59913ff0ad243e2b90ed5d85c1637e964dc
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-04-26 21:46:50 +0100

    Sort out login (I think)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,9 @@
-'use client';
-
-import {
-  signIn,
-  signOut,
-  useSession,
-  getProviders,
-  ClientSafeProvider,
-  LiteralUnion,
-} from 'next-auth/react';
-import { useState, useEffect } from 'react';
-
-// import connectDB from '@/config/database';
-
-type ProviderType = LiteralUnion<'Google' | 'GitHub' | string, string>;
-
-type ProviderInfo = Record<ProviderType, ClientSafeProvider> | null;
+import LoginButtons from '@/components/LoginButtons';
 
 export default function Home() {
-  // console.log('I am in the server?');
-  const { data: session } = useSession();
-  const [providers, setProviders] = useState<ProviderInfo>(null);
-
-  useEffect(() => {
-    const setAuthProviders = async () => {
-      const res = await getProviders();
-      setProviders(res);
-    };
-
-    setAuthProviders();
-  }, []);
-
-  // await connectDB();
-
   return (
-    <main className="flex justify-center items-center h-screen border gap-4">
-      {providers && !session ? (
-        Object.values(providers).map((provider, index) => (
-          <button
-            key={index}
-            onClick={() => signIn(provider.id)}
-            className="border py-2 px-4 text-2xl">
-            {provider.name}
-          </button>
-        ))
-      ) : (
-        <button onClick={() => signOut()} className="border py-2 px-4 text-2xl">
-          Logout
-        </button>
-      )}
+    <main className='flex justify-center items-center h-screen border gap-4'>
+      <LoginButtons />
     </main>
   );
 }
--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,17 +1,9 @@
 import connectDB from '@/config/database';
 import User from '@/models/User';
 
-import { Session, Profile } from 'next-auth';
+import { Session } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-
-function generateUsername(email: string): string {
-  if (!email.match(/^[^@]+@[^@]+\.[^@]+$/)) {
-    return 'defaultUser';
-  }
-  const atIndex = email.indexOf('@');
-  return email.substring(0, atIndex);
-}
 
 export const authOptions = {
   providers: [
@@ -31,27 +23,9 @@ export const authOptions = {
       },
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    // // Invoked on successful signin
-    // async signIn({ profile }: { profile: Profile }) {
-    //   // 1. Connect to database
-    //   await connectDB();
-    //   // 2. Check if user exists
-    //   const userExists = await User.findOne({ email: profile.email });
-    //   // 3 If not, then add user to database
-    //   if (!userExists) {
-    //     // Truncate user name if too long
-    //     const username =
-    //       profile.name?.slice(0, 20) || generateUsername(profile.email || '');
-    //     await User.create({
-    //       email: profile.email,
-    //       username,
-    //     });
-    //   }
-    //   // 4. Return true to allow sign in
-    //   return true;
     async signIn(response: any) {
-      console.log(response);
       const profile = response.profile;
       await connectDB();
       const userExists = await User.findOne({ email: profile.email });
@@ -61,13 +35,9 @@ export const authOptions = {
       }
       return true;
     },
-    // // Modifies the session object
     async session({ session }: { session: Session }) {
-      // 1. Get user from database
       const user = await User.findOne({ email: session.user.email });
-      // 2. Assign the user id to the session
       session.user.id = user._id.toString();
-      // 3. Return sesion
       return session;
     },
   },
