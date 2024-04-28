b350839a2dfcb294bdfc909c5b9452b4649e439f
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-04-28 14:38:09 +0100

    Re-remove login buttons for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import LoginButtons from '@/components/LoginButtons';
-
 export default function Home() {
   return (
     <main className='flex justify-center items-center h-screen border gap-4'>
-      <LoginButtons />
+      <div>
+        <h1>Hello</h1>
+      </div>
     </main>
   );
 }
