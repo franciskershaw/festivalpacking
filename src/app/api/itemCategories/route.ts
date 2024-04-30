8c72139329abf51bff8ca587fe249498755bb64e
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-04-30 20:37:35 +0100

    Write getSessionuser util and empty POST route

---
+++ src/app/api/itemCategories/route.ts
@@ -0,0 +1,6 @@
+import connectDB from '@/config/database';
+import ItemCategory from '@/models/ItemCategory';
+import getSessionUser from '@/utils/getSessionUser';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export const POST = async (request: NextApiRequest) => {};
