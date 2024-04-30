cda7e55aeef24d0e30f2d549111bfbce3104175b
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-04-30 21:21:29 +0100

    route for retrieving item categories

--- src/app/api/itemCategories/route.ts
+++ src/app/api/itemCategories/route.ts
@@ -1,6 +1,15 @@
 import connectDB from '@/config/database';
 import ItemCategory from '@/models/ItemCategory';
-import getSessionUser from '@/utils/getSessionUser';
-import { NextApiRequest, NextApiResponse } from 'next';
 
-export const POST = async (request: NextApiRequest) => {};
+export const GET = async () => {
+	try {
+		await connectDB();
+
+		const itemCategories = await ItemCategory.find().sort({ name: 1 });
+
+		return new Response(JSON.stringify(itemCategories));
+	} catch (error) {
+		console.log(error);
+		return new Response('Something went wrong', { status: 500 });
+	}
+};
