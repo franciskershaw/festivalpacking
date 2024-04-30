afbf182241a950791d1e580d242a345a61b461de
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-04-30 20:18:56 +0100

    Item and ItemCategory models

---
+++ src/models/ItemCategory.ts
@@ -0,0 +1,19 @@
+import { Schema, model, models } from 'mongoose';
+
+const ItemCategorySchema = new Schema({
+  name: {
+    type: String,
+    unique: [true, 'Item category already exists'],
+    required: [true, 'Name is required'],
+  },
+  faIcon: {
+    type: String,
+    unique: [true, 'Icon already in use'],
+    required: [true, 'Icon is required'],
+  },
+});
+
+const ItemCategory =
+  models.ItemCategory || model('ItemCategory', ItemCategorySchema);
+
+export default ItemCategory;
