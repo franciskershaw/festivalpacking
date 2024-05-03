0d180cb28484f77c0b2b34564f3195074ac21849
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-05-03 15:44:30 +0100

    GET route for items

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -1,6 +1,7 @@
 import connectDB from '@/config/database';
 import Item from '@/models/Item';
 import getSessionUser from '@/utils/getSessionUser';
+import { ObjectId } from 'mongodb';
 
 export const POST = async (request: Request) => {
 	try {
@@ -31,6 +32,35 @@ export const POST = async (request: Request) => {
 		return new Response(JSON.stringify({ newItem }), {
 			status: 200,
 		});
+	} catch (error) {
+		console.log(error);
+		return new Response('Something went wrong', { status: 500 });
+	}
+};
+
+export const GET = async (request: Request) => {
+	try {
+		await connectDB();
+
+		const sessionUser = await getSessionUser();
+
+		let filter: {
+			approved?: boolean;
+			$or?: Array<{ approved: boolean } | { createdBy: ObjectId }>;
+		} = { approved: true };
+
+		if (sessionUser) {
+			filter = {
+				$or: [
+					{ approved: true },
+					{ createdBy: new ObjectId(sessionUser.user._id) },
+				],
+			};
+		}
+
+		const items = await Item.find(filter);
+
+		return new Response(JSON.stringify({ items }));
 	} catch (error) {
 		console.log(error);
 		return new Response('Something went wrong', { status: 500 });
