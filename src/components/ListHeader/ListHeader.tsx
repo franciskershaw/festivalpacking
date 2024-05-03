8a919e7a5c6aed0be86c01ac11856b7bb3b5b113
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-05-03 16:17:56 +0100

    Simple SearchBar, fix spacing

--- src/components/ListHeader/ListHeader.tsx
+++ src/components/ListHeader/ListHeader.tsx
@@ -2,19 +2,20 @@
 
 import { useList } from '@/providers/ListContext';
 
+import SearchBar from '../SearchBar/SearchBar';
 import TextInput from '../TextInput/TextInput';
 
 const ListHeader = () => {
 	const { dispatch } = useList();
 	return (
-		<header className="flex justify-between items-center py-8">
+		<header className="grid grid-cols-2 gap-6 py-4 overflow-hidden">
 			<TextInput
 				onChange={(name) =>
 					dispatch({ type: 'SET_FESTIVAL_NAME', payload: name })
 				}
 				placeholder="New Festival"
 			/>
-			{/* <p>Search bar</p> */}
+			<SearchBar />
 		</header>
 	);
 };
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+const SearchBar = () => {
+	return (
+		<input
+			className="border border-gray-300 rounded-md w-full pl-2 py-1 focus:border-gray-500 focus:outline-none"
+			type="text"
+		/>
+	);
+};
+
+export default SearchBar;
