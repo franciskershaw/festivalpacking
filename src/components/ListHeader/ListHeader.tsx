7b1a1594d670729e34b5cf8ec563354dd65d063d
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-05-03 21:22:59 +0100

    Remove comments

--- src/components/ListHeader/ListHeader.tsx
+++ src/components/ListHeader/ListHeader.tsx
@@ -1,4 +1,3 @@
-// 'use client';
 import { getItems } from '@/server/actions';
 
 import FestivalName from '../FestivalName/FestivalName';
@@ -6,7 +5,7 @@ import SearchBar from '../SearchBar/SearchBar';
 
 const ListHeader = async () => {
 	const allItems = await getItems();
-	// const { dispatch } = useList();
+
 	return (
 		<header className="relative grid grid-cols-2 gap-6 py-4">
 			<FestivalName />
