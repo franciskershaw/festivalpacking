9b98631908f477f574c6de2c65e4704362b43d29
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-05-05 10:49:25 +0100

    Capitalise item search results

--- src/components/SearchBar/SearchResult.tsx
+++ src/components/SearchBar/SearchResult.tsx
@@ -5,7 +5,7 @@ const SearchResult = ({ item, border }: { item: Item; border: boolean }) => {
 		<div
 			className={`flex justify-between items-center py-4 px-3 hover:bg-gray-100 border-0 ${border ? 'border-b-2' : ''} `}
 		>
-			<p className="">{item.name}</p>
+			<p className="capitalize">{item.name}</p>
 			<p className="text-gray-500">{item.category.name}</p>
 		</div>
 	);
