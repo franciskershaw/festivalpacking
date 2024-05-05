bedac7950cd3c3b2b55d0a8ecec40b3f0bb4e4bf
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-05-05 15:55:19 +0100

    Filter out results already in context

--- src/components/SearchBar/useSearchBar.tsx
+++ src/components/SearchBar/useSearchBar.tsx
@@ -1,29 +1,47 @@
 import { useMemo, useState } from 'react';
 
+import { useList } from '@/providers/ListContext';
 import { Item } from '@/utils/types';
 
 const useSearchBar = (allItems: Item[]) => {
 	const [search, setSearch] = useState('');
 
+	const { state } = useList();
+
+	const excludedIds = useMemo(
+		() => new Set(state.items.map((item) => item._id)),
+		[state.items],
+	);
+
 	const searchResults = useMemo(() => {
 		if (search === '') {
 			return [];
 		}
+
 		const lowerCaseSearch = search.toLowerCase();
-		const startsWithResults = allItems.filter((item) =>
-			item.name.toLocaleLowerCase().startsWith(lowerCaseSearch),
+
+		const startsWithResults = allItems.filter(
+			(item) =>
+				item.name.toLowerCase().startsWith(lowerCaseSearch) &&
+				!excludedIds.has(item._id),
 		);
+
 		if (startsWithResults.length >= 5) {
 			return startsWithResults.slice(0, 5);
 		}
-		const includesResults = allItems.filter((item) =>
-			item.name.toLowerCase().includes(lowerCaseSearch),
+
+		const includesResults = allItems.filter(
+			(item) =>
+				item.name.toLowerCase().includes(lowerCaseSearch) &&
+				!excludedIds.has(item._id),
 		);
+
 		const combinedResults = Array.from(
 			new Set([...startsWithResults, ...includesResults]),
 		);
+
 		return combinedResults.slice(0, 5);
-	}, [allItems, search]);
+	}, [allItems, search, excludedIds]);
 
 	return { search, setSearch, searchResults };
 };
