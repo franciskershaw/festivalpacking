766135bb3c70e9b30ee047efa96fb1699c8b1772
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-05-05 13:06:29 +0100

    Add item into context

--- src/components/SearchBar/SearchResult.tsx
+++ src/components/SearchBar/SearchResult.tsx
@@ -1,13 +1,16 @@
+import { useList } from '@/providers/ListContext';
 import { Item } from '@/utils/types';
 
 import Icon from '../Icon/Icon';
 
 const SearchResult = ({ item, border }: { item: Item; border: boolean }) => {
+	const { dispatch } = useList();
 	return (
 		<div
 			className={`flex justify-between items-center py-5 px-3 hover:bg-gray-100 border-0 ${border ? 'border-b-2' : ''} `}
 			role="button"
 			tabIndex={0}
+			onClick={() => dispatch({ type: 'ADD_ITEM', payload: item })}
 		>
 			<p className="capitalize">{item.name}</p>
 			<div className="flex items-center gap-4">
--- src/providers/ListContext.tsx
+++ src/providers/ListContext.tsx
@@ -2,15 +2,24 @@
 
 import { createContext, useContext, useEffect, useReducer } from 'react';
 
+import { Item } from '@/utils/types';
+
 // Types
 type ListState = {
 	festivalName: string;
+	items: Item[];
 };
-type Action = { type: 'SET_FESTIVAL_NAME'; payload: string };
+type Action =
+	| {
+			type: 'SET_FESTIVAL_NAME';
+			payload: string;
+	  }
+	| { type: 'ADD_ITEM'; payload: Item };
 
 // Initial state
 const initialState: ListState = {
 	festivalName: '',
+	items: [],
 };
 
 // Create context
@@ -27,6 +36,8 @@ function listReducer(state: ListState, action: Action): ListState {
 	switch (action.type) {
 		case 'SET_FESTIVAL_NAME':
 			return { ...state, festivalName: action.payload };
+		case 'ADD_ITEM':
+			return { ...state, items: [...state.items, action.payload] };
 		default:
 			return state;
 	}
