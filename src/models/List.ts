57f55ad578c4ff7523f21f72337620e3a09d5df4
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-05-08 15:58:03 +0100

    Update list model to include timestamps

--- src/models/List.ts
+++ src/models/List.ts
@@ -1,27 +1,32 @@
 import { Schema, model, models } from 'mongoose';
 
-const ListSchema = new Schema({
-	name: {
-		type: String,
-		unique: [true, 'Item category already exists'],
-		required: [true, 'Name is required'],
+const ListSchema = new Schema(
+	{
+		name: {
+			type: String,
+			unique: [true, 'Item category already exists'],
+			required: [true, 'Name is required'],
+		},
+		items: {
+			type: [
+				{
+					_id: Schema.Types.ObjectId,
+					obtained: Boolean,
+				},
+			],
+			required: true,
+			default: [],
+		},
+		createdBy: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
 	},
-	items: {
-		type: [
-			{
-				_id: Schema.Types.ObjectId,
-				obtained: Boolean,
-			},
-		],
-		required: true,
-		default: [],
+	{
+		timestamps: true,
 	},
-	createdBy: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-		required: true,
-	},
-});
+);
 
 const List = models.List || model('List', ListSchema);
 
