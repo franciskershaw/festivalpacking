9189152d90325d9ebb5e641cfce68c34e8339ea3
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-05-08 19:06:46 +0100

    Basic no lists saved state

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { getUserLists } from '@/server/actions';
 import { List } from '@/utils/types';
 
@@ -8,32 +10,39 @@ import EditList from './client/EditList';
 const ListsPage = async () => {
 	const res = await getUserLists();
 	const data = res.data;
+	console.log(data.length === 0);
 
 	return (
 		<div className="pt-6 space-y-4">
 			<h1 className="text-2xl">Saved lists</h1>
-			{data?.map((list: List) => (
-				<div
-					className="flex items-center justify-between border-0 border-b-2 pb-2"
-					key={list._id}
-				>
-					<div className="space-y-1">
-						<h2 className="text-lg">{list.name}</h2>
-						<h3 className="text-xs">
-							Created: {new Date(list.createdAt).toLocaleDateString()}
-						</h3>
-						<h3 className="text-xs">
-							Updated: {new Date(list.updatedAt).toLocaleDateString()}
-						</h3>
-					</div>
-
-					{/* Buttons */}
-					<div className="flex items-center gap-4">
-						<EditList list={list} />
-						<Icon name="FaTrashCan" size={20} />
-					</div>
+			{data?.length === 0 ? (
+				<div className="flex flex-col items-center justify-center h-40">
+					<h2>No lists saved yet</h2>
 				</div>
-			))}
+			) : (
+				data?.map((list: List) => (
+					<div
+						className="flex items-center justify-between border-0 border-b-2 pb-2"
+						key={list._id}
+					>
+						<div className="space-y-1">
+							<h2 className="text-lg">{list.name}</h2>
+							<h3 className="text-xs">
+								Created: {new Date(list.createdAt).toLocaleDateString()}
+							</h3>
+							<h3 className="text-xs">
+								Updated: {new Date(list.updatedAt).toLocaleDateString()}
+							</h3>
+						</div>
+
+						{/* Buttons */}
+						<div className="flex items-center gap-4">
+							<EditList list={list} />
+							<Icon name="FaTrashCan" size={20} />
+						</div>
+					</div>
+				))
+			)}
 		</div>
 	);
 };
