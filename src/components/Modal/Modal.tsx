c55cdc37e0a13b467a89d55bfdba4832f16f4cd3
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-05-12 10:51:16 +0100

    Revised modal component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,23 +1,30 @@
 import { ReactNode } from 'react';
 
+import Link from 'next/link';
+
 import Icon from '../Icon/Icon';
 
-export default function Modal({ children }: { children: ReactNode }) {
+export default function Modal({
+	children,
+	href,
+}: {
+	children: ReactNode;
+	href: string;
+}) {
 	return (
 		<div
 			role="presentation"
-			className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm transition-opacity duration-300 ease-in-out"
+			className="fixed inset-0 z-50 flex items-end sm:items-center justify-center bg-gray-600 bg-opacity-50 backdrop-blur-sm transition-opacity duration-300 ease-in-out"
 		>
-			<dialog className="relative w-full max-w-md rounded-lg bg-white p-6 shadow-lg dark:bg-gray-950">
-				<button
-					className="absolute top-4 right-4 rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-					type="button"
-				>
-					<span className="sr-only">Close</span>
-					<Icon name="FaXmark" size={12} />
+			<div className="relative w-full max-w-md rounded-lg bg-white p-6 shadow-lg mb-2 md:mb-0">
+				<button className="absolute top-2 right-4 rounded-md p-2" type="button">
+					<Link href={href}>
+						<span className="sr-only">Close</span>
+						<Icon name="FaXmark" size={24} />
+					</Link>
 				</button>
-				{children}
-			</dialog>
+				<div className="p-2">{children}</div>
+			</div>
 		</div>
 	);
 }
