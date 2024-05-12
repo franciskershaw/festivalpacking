3e1c3441ba81b1aaf0d9b918a08d57846c4c0f93
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-05-12 12:15:04 +0100

    Make modal dialog more semantic

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -16,7 +16,10 @@ export default function Modal({
 			role="presentation"
 			className="fixed inset-0 z-50 flex items-end sm:items-center justify-center bg-gray-600 bg-opacity-50 backdrop-blur-sm transition-opacity duration-300 ease-in-out"
 		>
-			<div className="relative w-full max-w-md rounded-lg bg-white p-6 shadow-lg my-2 mx-2 md:mb-0">
+			<dialog
+				open
+				className="relative w-full max-w-md rounded-lg bg-white p-6 shadow-lg my-2 mx-2 md:mb-0"
+			>
 				<button className="absolute top-2 right-4 rounded-md p-2" type="button">
 					<Link href={href}>
 						<span className="sr-only">Close</span>
@@ -24,7 +27,7 @@ export default function Modal({
 					</Link>
 				</button>
 				<div className="p-2 mt-3">{children}</div>
-			</div>
+			</dialog>
 		</div>
 	);
 }
