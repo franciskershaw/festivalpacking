d4afe0c4c8d5437c24cf2935ca5d22da5dc9455e
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-05-18 10:11:17 +0100

    Fix cursor behaviour of accordion

--- src/components/PackingList/CategoryHeader.tsx
+++ src/components/PackingList/CategoryHeader.tsx
@@ -4,9 +4,11 @@ import Icon from '../Icon/Icon';
 
 const CategoryHeader = ({ category }: { category: Category }) => {
 	return (
-		<div className="flex items-center gap-4">
+		<div className="flex items-center gap-4 cursor-default">
 			<Icon name={category.faIcon} size={20} />
-			<span className='text-lg font-bold border-0 border-b md:text-xl'>{category.name}</span>
+			<span className="text-lg font-bold border-0 border-b cursor-text md:text-xl">
+				{category.name}
+			</span>
 		</div>
 	);
 };
