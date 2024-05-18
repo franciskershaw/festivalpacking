4d553b7cd94e1fb112bddd61de1d7425a40ee8d8
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-05-18 10:06:24 +0100

    Fix hover effect

--- src/components/SearchBar/SearchResult.tsx
+++ src/components/SearchBar/SearchResult.tsx
@@ -25,14 +25,14 @@ const SearchResult = ({
 	};
 	return (
 		<div
-			className={`flex justify-between items-center py-5 px-3 hover:bg-gray-100 border-0 ${border ? 'border-b-2' : ''} `}
+			className={`flex justify-between items-center py-5 px-3 hover:bg-gray-100 border-0 hover:text-indigo-900 ${border ? 'border-b-2' : ''} `}
 			role="button"
 			tabIndex={0}
 			onClick={onClick}
 		>
 			<p className="capitalize">{item.name}</p>
 			<div className="flex items-center gap-4">
-				<p className="text-gray-500">{item.category.name}</p>
+				<p className="text-gray-400">{item.category.name}</p>
 				<Icon name={item.category.faIcon} size={20} />
 			</div>
 		</div>
